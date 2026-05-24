203183794fe92b6560df69ca785e67a3438b5fb1
JosephCortez15 <superjuniorjwh@gmail.com>
2026-05-23 20:53:31 -0400

    Nuevas opciones de uso en main

--- backend-java/Main.java
+++ backend-java/Main.java
@@ -20,12 +20,12 @@ public class Main {
             System.out.println("2. Registrar nuevo usuario");
             System.out.println("3. Ver cátalogo de productos");
             System.out.println("4. Comprar un producto (Agregar al carrito)");
-            System.out.println("5. Salir");
+            System.out.println("5. Lista de deseos");
+            System.out.println("6. Ir a la caja (Pagar)");
+            System.out.println("7. Salir");
             System.out.println("Elige una opcion: ");
-
             opcion = teclado.nextInt();
             teclado.nextLine();
-
             if (opcion == 1) {
                 System.out.println("\n--- Lista de Uusarios ---");
                 for (Usuario u : tablaUsuarios){
@@ -34,24 +34,17 @@ public class Main {
             } else if (opcion == 2) {
                 System.out.println("\n--- Registro de Usuarios ---");
                 Usuario nuevo = new Usuario();
-
                 nuevo.setIdUsuario(tablaUsuarios.size() + 1);
                 nuevo.setIdRol(2);
-
                 System.out.println("Ingresa tu nombre");
                 nuevo.setNombre(teclado.nextLine());
-
                 System.out.println("Ingresa tu apellido");
                 nuevo.setApellido(teclado.nextLine());
-
                 System.out.println("Ingresa tu correo");
                 nuevo.setCorreo(teclado.nextLine());
-
                 System.out.println("Ingresa tu contrasena");
                 nuevo.setContrasena(teclado.nextLine());
-
                 nuevo.setFechaRegistro("2026-05-19");
-
                 tablaUsuarios.add(nuevo);
                 System.out.println("Usuario registrado con exito!");
             }   else if (opcion == 3) {
@@ -63,7 +56,6 @@ public class Main {
                 System.out.println("Ingresa el ID del producto que deseas comprar");
                 int idBuscado = teclado.nextInt();
                 Producto productoEncontrado = null;
-
                 for (Producto p : tablaProductos){
                     if (p.getIdProducto() == idBuscado) {
                         productoEncontrado = p;
@@ -78,10 +70,50 @@ public class Main {
                 }   else {
                     System.out.println("Error: No existe ningun producto con ID en el catalogo");
                 }
-            }   else if (opcion == 5){
+            }   else if (opcion == 5) {
+                System.out.println("\n --- Agregar a lista de deseos ---");
+                System.out.println("Ingresa el id del producto que te gusta: ");
+                int idBuscado = teclado.nextInt();
+                teclado.nextLine();
+                Producto productoEncontrado = null;
+                for (Producto p : tablaProductos){
+                    if (p.getIdProducto() == idBuscado) {
+                        productoEncontrado = p;
+                        break;
+                    }
+                }
+                if (productoEncontrado != null) {
+                    Usuario comprador = tablaUsuarios.get(0);
+                    comprador.agregarADeseos(productoEncontrado);
+                    System.out.println(productoEncontrado.getNombre() + ". Se agrego a tus favoritos");
+                    comprador.verListaDeseos();
+                }   else {
+                    System.out.println("Error: No existe ningun producto con este ID");
+                }
+            }   else if (opcion == 6) {
+                System.out.println("\n--- Caja registradora ---");
+                Usuario comprador = tablaUsuarios.get(0);
+                if (comprador.tieneCarritoVacio()) {
+                    System.out.println("Tu carrito esta vacio. Ve al carrito para añadir un producto");
+                }   else {
+                    comprador.verCarrito();
+                    double totalAPagar = comprador.calcularTotalCarrito();
+                    System.out.println("\n --- Deseas confirmar tu pago por: " + totalAPagar + "? (Presione 1 para SI / 2 para NO)");
+                    int confirmacion = teclado.nextInt();
+                    teclado.nextLine();
+                    if (confirmacion == 1) {
+                            System.out.println("\n Procesando pago...");
+                            System.out.println("Pago exitoso! Se ha generado su pedido.");
+                            System.out.println("Pasando al area de preparacion y envio.");
+                            comprador.vaciarCarrito();
+                    } else if (confirmacion == 2) {
+                        System.out.println("Compra cancelada. Tus productos siguen guardados en el carrito.");
+                    }   else{
+                        System.out.println("\n Opcion no valida. Cancelando operacion.");
+                    }
+                }
+            }   else if (opcion == 5) {
                 System.out.println("Saliendo del sistema");
-            }   else {
-                System.out.println("Opcion no valida. Intente de nuevo");
             }
         }
         teclado.close();
--- backend-java/Usuario.java
+++ backend-java/Usuario.java
@@ -11,9 +11,11 @@ public class Usuario {
     private String contrasena;
     private String fechaRegistro;
     private List<Producto> carrito;
+    private List<Producto> listaDeseos;
 
     public Usuario(){
         this.carrito = new ArrayList<>();
+        this.listaDeseos = new ArrayList<>();
     }
 
     public Usuario(int idUsuario, int idRol, String nombre, String apellido, String correo, String contrasena, String fechaRegistro){
@@ -25,6 +27,7 @@ public class Usuario {
         this.contrasena = contrasena;
         this.fechaRegistro = fechaRegistro;
         this.carrito = new ArrayList<>();
+        this.listaDeseos = new ArrayList<>();
     }
 
     public int getIdUsuario() {
@@ -99,6 +102,37 @@ public class Usuario {
             }
             System.out.println("Total a pagar = " + total);
         }
+    }
+
+    public void agregarADeseos(Producto p){
+        this.listaDeseos.add(p);
+    }
+
+    public void verListaDeseos(){
+        System.out.println("\n--- Lista de deseos de " + this.nombre + " ---");
+        if (listaDeseos.isEmpty()) {
+            System.out.println("Tu lista de deseos esta vacia");
+        } else {
+            for (Producto p : listaDeseos){
+                System.out.println(" " + p.getNombre() + " (" + p.getMarca() + " )");
+            }
+        }
+    }
+
+    public double calcularTotalCarrito(){
+        double total = 0;
+        for (Producto p : carrito){
+            total += p.getPrecioBase();
+        }
+        return total;
+    }
+
+    public boolean tieneCarritoVacio(){
+        return this.carrito.isEmpty();
+    }
+
+    public void vaciarCarrito(){
+        this.carrito.clear();
     }
 
     @Override
